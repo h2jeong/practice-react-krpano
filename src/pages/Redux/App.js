6a14f9dcd81f10b63fb44bbed0329ce79fc90c68
Zoe <heejeong.cho@stryx.co.kr>
2021-06-18 10:51:18 +0900

    redux page

--- src/pages/Redux/App.js
+++ src/pages/Redux/App.js
@@ -1,7 +1,9 @@
 /* eslint-disable no-unused-vars */
 import { Hotspot, Krpano, Scene } from '@0xllllh/react-krpano';
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import * as api from './api';
+import { addHotspot, addScene, changeCurrentScene, loadPano } from './reducer';
 
 const ReduxApp = () => {
   const currentState = useSelector((state) => state);
@@ -9,21 +11,80 @@ const ReduxApp = () => {
   const scenes = useSelector((state) => state.scenes.ids.map((id) => state.scenes.byId[id]));
   const hotspotCount = useSelector((state) => state.hotspots.ids.length);
   const hotspotById = useSelector((state) => state.hotspots.byId);
-  const dispatch = useDispatch();
   const [renderer, setRenderer] = useState(null);
+  const dispatch = useDispatch();
+
+  const onReady = useCallback((renderer) => {
+    setRenderer(renderer);
+    api.getPanoState().then((pano) => {
+      dispatch(loadPano(pano));
+    });
+  }, []);
+  const onAddHotspot = useCallback(() => {
+    console.log(renderer);
+    dispatch(
+      addHotspot({
+        name: `hotspot${hotspotCount + 1}`,
+        ath: renderer?.get('view.hlookat') || 0,
+        atv: renderer?.get('view.vlookat') || 0,
+        style: 'hotspot-style'
+      })
+    );
+  }, [renderer, hotspotCount]);
+  const onSave = useCallback(() => {
+    api.savePanoState(currentState).then(() => {
+      window.alert('pano saved');
+    });
+  }, [currentState]);
+  const handleChangeScene = (name) => {
+    dispatch(changeCurrentScene(name));
+  };
+  const onAddScene = useCallback(() => {
+    api.generateNewScene().then((sc) => {
+      dispatch(addScene(sc));
+      alert('new scene added');
+    });
+  }, []);
+
   return (
     <>
       <Krpano
         xml={`${process.env.PUBLIC_URL}/xml/dynamic-hotspot.xml`}
         className="pano"
         currentScene={currentScene}
-        // eslint-disable-next-line no-undef
         onReady={onReady}
         enableLogger={true}>
-        <Scene>
-          <Hotspot />
-        </Scene>
+        {scenes.map((scene) => (
+          <Scene key={scene.name} {...scene}>
+            {scene.hotspotIds
+              .map((name) => hotspotById[name])
+              .map((hs) => (
+                <Hotspot key={hs.name} {...hs} />
+              ))}
+          </Scene>
+        ))}
       </Krpano>
+      <div className="action-bar">
+        <button className="action-button" onClick={onAddHotspot}>
+          +
+        </button>
+        <button className="action-button" onClick={onSave}>
+          S
+        </button>
+      </div>
+      <div className="scene-list">
+        {scenes.map((sc) => (
+          <button
+            key={sc.name}
+            className={`scene-list-item ${sc.name === currentScene ? 'active' : ''}`}
+            onClick={handleChangeScene(sc.name)}>
+            <img src={sc.previewUrl || sc.images[0].url.replace('%s', 'f')} alt={sc.name} />
+          </button>
+        ))}
+        <button className="scene-list-item scene-list-item--add" onClick={onAddScene}>
+          +
+        </button>
+      </div>
     </>
   );
 };
